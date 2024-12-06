1c2a9fa8eed8341d2f00482c0802071f277aadb5
Zhang Yufeng <759094438@qq.com>
2024-12-06 11:28:52 +0800

    [ADD] update 02 demo with casadi position control solver

--- 02-first-example.py
+++ 02-first-example.py
@@ -1,5 +1,6 @@
 import time
 import unittest
+import threading
 import example_robot_data
 import numpy as np
 import casadi
@@ -12,11 +13,10 @@ model = robot.model
 data = robot.data
 
 viz = MeshcatVisualizer(robot, url=None)
-# viz = MeshcatVisualizer(robot.model, robot.collision_model, robot.visual_model)
-viz.initViewer(loadModel=True, open=True)
 viz.display(robot.q0)
 
 robot.q0 = np.array([0, -np.pi / 2, 0, 0, 0, 0])
+q = robot.q0
 
 tool_id = model.getFrameId("tool0")
 
@@ -29,28 +29,13 @@ in_world_M_target = pin.SE3(
 
 # Add a vizualization for the target
 boxID = "world/box"
-# material = materialFromColor([1.0, 0.2, 0.2, 0.5])
-# viz.viewer[boxID].set_object(meshcat.geometry.Box([0.05, 0.1, 0.2]), material)
 viz.addBox(boxID, [0.05, 0.1, 0.2], [1.0, 0.2, 0.2, 0.5])
 
 # Add a vizualisation for the tip of the arm.
 tipID = "world/blue"
 viz.addBox(tipID, [0.08] * 3, [0.2, 0.2, 1.0, 0.5])
-# material = materialFromColor([0.2, 0.2, 1.0, 0.5])
-# viz.viewer[tipID].set_object(meshcat.geometry.Box([0.08] * 3), material)
 
-viz.applyConfiguration(tipID, in_world_M_target)
-# R, p = in_world_M_target.rotation, in_world_M_target.translation
-# T = np.r_[np.c_[R, p], [[0, 0, 0, 1]]]
-# viz.viewer[tipID].set_transform(T)
-
-# --- Casadi helpers
-cmodel = cpin.Model(model)
-cdata = cmodel.createData()
-cq = casadi.SX.sym("q", model.nq, 1)
-print(3 * (cq + 1))
-cpin.framesForwardKinematics(cmodel, cdata, cq)
-print(cdata.oMf[tool_id])
+lock = threading.Lock()
 
 
 def displayScene(q):
@@ -68,5 +53,69 @@ def displayScene(q):
     time.sleep(1e-1)
 
 
+# Function to update the visualizer in a separate thread
+def visualizer_thread():
+    while True:
+        with lock:
+            viz.display(q)  # Update the visualizer with the latest configuration
+        time.sleep(0.05)  # Adjust for desired update rate
+
+
+# --- Casadi helpers
+cmodel = cpin.Model(model)
+cdata = cmodel.createData()
+cq = casadi.SX.sym("q", model.nq, 1)
+# cpin.framesForwardKinematics(cmodel, cdata, cq)
+
+# Start the visualizer thread
+thread = threading.Thread(target=visualizer_thread, daemon=True)
+thread.start()
+
+curr_time = 0.0
+
 while True:
-    viz.display(robot.q0)
+    # set target pos
+    delta_x = 0.1 * np.sin(curr_time)
+    delta_y = 0.1 * np.sin(curr_time)
+    in_world_M_target = pin.SE3(
+        pin.utils.rotate("x", np.pi / 4),
+        np.array([-0.5, 0.1 + delta_y, 0.2]),
+    )
+    curr_time += 0.1
+
+    # --- Casadi helpers
+    cmodel = cpin.Model(model)
+    cdata = cmodel.createData()
+    cq = casadi.SX.sym("q", model.nq, 1)
+    cpin.framesForwardKinematics(cmodel, cdata, cq)
+
+    # setup optimization
+    error_tool = casadi.Function(
+        "etool",
+        [cq],
+        [cpin.log6(cdata.oMf[tool_id].inverse() * cpin.SE3(in_world_M_target)).vector],
+    )
+    opti = casadi.Opti()
+    var_q = opti.variable(model.nq)
+    opti.set_initial(var_q, q)
+    totalcost = casadi.sumsqr(error_tool(var_q))
+    opti.minimize(totalcost)
+    opti.solver("ipopt")  # select the backend solver
+    opti.callback(lambda i: displayScene(opti.debug.value(var_q)))
+
+    # calculate a solution
+    try:
+        sol = opti.solve_limited()
+        sol_q = opti.value(var_q)
+    except:
+        print("ERROR in convergence, plotting debug info.")
+        sol_q = opti.debug.value(var_q)
+
+    pin.framesForwardKinematics(model, data, sol_q)
+    opti.value(error_tool(var_q)), pin.log(data.oMf[tool_id].inverse() * in_world_M_target).vector
+
+    with lock:
+        q = sol_q
+        # robot.q0 = q
+        displayScene(sol_q)
+    time.sleep(0.05)
